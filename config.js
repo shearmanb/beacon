be8cad76416d2986d642a99f97f623c6b7f35480
shearmanb <bjanssen@gmail.com>
2026-05-18 10:04:50 -0400

    chore: set schedule for fountain_inn_dc to working_hours_heavy [skip ci]

--- config.js
+++ config.js
@@ -57,7 +57,7 @@ export const sites = [
     strategy: "shopify_collection",
     url: "https://shop.fountaininndc.com",
     intervalMinutes: 30,
-    schedule: "60",
+    schedule: "working_hours_heavy",
     imminentIntervalMinutes: 2,
     imminent: false,
     alertOnNewProduct: true,
