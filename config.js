43c34c2bed35904fc1c7ab60b5e256b2344be989
shearmanb <bjanssen@gmail.com>
2026-05-17 22:23:49 -0400

    chore: set schedule for sharedpour_reveries to 60 [skip ci]

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ export const sites = [
     strategy: "shopify_collection",
     url: "https://sharedpour.com",
     intervalMinutes: 20,
-    schedule: "20",
+    schedule: "60",
     imminentIntervalMinutes: 2,
     imminent: false,
     alertOnNewProduct: true,
