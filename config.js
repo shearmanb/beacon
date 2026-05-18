00e08d497fc5ed8856649425f65be356efda44ab
shearmanb <bjanssen@gmail.com>
2026-05-18 13:38:33 -0400

    chore: set schedule for bourbon_concierge to 15 [skip ci]

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ export const sites = [
     strategy: "shopify_collection",
     url: "https://thebourbonconcierge.com",
     intervalMinutes: 30,
-    schedule: "working_hours_heavy",
+    schedule: "15",
     imminentIntervalMinutes: 2,
     imminent: false,
     alertOnNewProduct: true,
