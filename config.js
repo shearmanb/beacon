eaf2cc6f1cea8e4270d299b2e3fdc595daae2902
shearmanb <bjanssen@gmail.com>
2026-05-21 23:30:44 -0400

    chore: set schedule for reveries_official to 20 [skip ci]

--- config.js
+++ config.js
@@ -136,7 +136,7 @@ export const sites = [
     storefrontAccessToken: "9238d5183f01462f5ba642447811ebb8",
     storefrontCollectionId: "367215214747",
     intervalMinutes: 30,
-    schedule: "15",
+    schedule: "20",
     imminentIntervalMinutes: 2,
     imminent: false,
     alertOnNewProduct: true,
